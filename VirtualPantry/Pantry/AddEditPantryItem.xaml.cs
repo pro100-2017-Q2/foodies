9e9f06e1e3e3212b3a829d5e72894352bf92715c
DallasLewis1996 <dallaslewis1996@gmail.com>
2017-05-19 18:00:31 -0600

    git fixes

--- VirtualPantry/Pantry/AddEditPantryItem.xaml.cs
+++ VirtualPantry/Pantry/AddEditPantryItem.xaml.cs
@@ -61,19 +61,12 @@ namespace VirtualPantry.Pantry
         {
             
         }
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
 
         private void cancelButton_Click(object sender, RoutedEventArgs e)
         {
             
         }
-=======
-=======
->>>>>>> origin/development
->>>>>>> origin/development
->>>>>>> origin/development
+
     }
 }
