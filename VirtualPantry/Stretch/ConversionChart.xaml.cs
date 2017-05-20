b220896bd6af7d3d4ed54a3c9ed91eb64bf265e0
Mitch <makurmit@gmail.com>
2017-05-19 18:08:07 -0600

    Added timer and conversion chart navigation
    also added a cache file to the git ignore, hopefully that won't break
    anything...

--- VirtualPantry/Stretch/ConversionChart.xaml.cs
+++ VirtualPantry/Stretch/ConversionChart.xaml.cs
@@ -25,5 +25,11 @@ namespace VirtualPantry.Stretch
             InitializeComponent();
             main = mainWindow;
         }
-    }
+
+		private void Home_Button_Click(object sender, RoutedEventArgs e)
+		{
+			main.Show();
+			this.Hide();
+		}
+	}
 }
