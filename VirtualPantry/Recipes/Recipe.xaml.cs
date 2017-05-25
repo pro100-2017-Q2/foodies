14ad8f0d110f04891b31082303f083fa2442c360
jjennings <jjennings@student.neumont.edu>
2017-05-24 18:07:59 -0600

    Worked on Recipe Page
    I think I set up the binding for the file contents to be read in and
    organized. I think I allowed the image to be shown as well.

--- VirtualPantry/Recipes/Recipe.xaml.cs
+++ VirtualPantry/Recipes/Recipe.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace VirtualPantry.Recipes
 {
@@ -20,11 +8,44 @@ namespace VirtualPantry.Recipes
     public partial class Recipe : Window
     {
         public MainWindow main;
+
+        public string FileText
+        {
+            get
+            {
+                return FileText;
+            }
+            set
+            {
+                FileText = value;
+                return;
+            }
+        }
+            
+        public object RecipeListing
+        {
+            get
+            {
+                return RecipeListing;
+            }
+            set
+            {
+                RecipeListing = value;
+                return;
+            }
+        }
+
+        public void ReadFile(string path)
+        {
+            RecipeListing = path;
+        }
+
         public Recipe(MainWindow mainWindow)
         {
             InitializeComponent();
             main = mainWindow;
         }
+
         private void homeButton_Clicked(object sender, RoutedEventArgs e)
         {
             main.Show();
