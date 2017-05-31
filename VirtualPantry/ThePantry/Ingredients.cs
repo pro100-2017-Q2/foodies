5e70f0e7cbc4bf0fb17067a4598c69104a36d372
jjennings <jjennings@student.neumont.edu>
2017-05-30 18:09:01 -0600

    Resolved Errors

--- VirtualPantry/ThePantry/Ingredients.cs
+++ VirtualPantry/ThePantry/Ingredients.cs
@@ -11,9 +11,9 @@ using VirtualPantry.Enums;
 namespace VirtualPantry.ThePantry
 {
 
-	public class Ingredients 
-	{
-<<<<<<< HEAD
+    public class Ingredients
+    {
+
         public Ingredients(string name, string wholeNumber, List<string> fractions, Categories categories, Units units)
         {
             Name = name;
@@ -28,58 +28,5 @@ namespace VirtualPantry.ThePantry
         public List<string> Fractions { get; set; }
         public Categories Categories { get; set; }
         public Units Units { get; set; }
-    }
-=======
-		private string name;
-		private int wholeNumber;
-		private Categories category;
-		private Units ingredientUnits;
-
-		public string Name
-		{
-			get { return name; }
-			set { name = value; FieldChanged(); }
-		}
-		public int WholeNumber
-		{
-			get { return wholeNumber; }
-			set { wholeNumber = value; FieldChanged(); }
-		}
-		public Categories Category
-		{
-			get { return category; }
-			set { category = value; FieldChanged(); }
-		}
-		public Units IngredientUnits
-		{
-			get { return ingredientUnits; }
-			set { ingredientUnits = value; FieldChanged(); }
-		}
-		public Ingredients(string name, int wholeNumber, Categories category, Units units)
-		{
-			this.Name = name;
-			this.WholeNumber = wholeNumber;
-			this.Category = category;
-			this.IngredientUnits = units;
-		}
-		public Ingredients()
-		{
-
-		}
-
-		public override string ToString()
-		{
-			return $"{Name} {wholeNumber} {ingredientUnits}(s)";
-		}
-
-		public event PropertyChangedEventHandler PropertyChanged;
-		protected void FieldChanged([CallerMemberName] string field = null)
-		{
-			if (PropertyChanged != null)
-			{
-				PropertyChanged(this, new PropertyChangedEventArgs(field));
-			}
-		}
-	}
->>>>>>> origin/development
+    }  
 }
