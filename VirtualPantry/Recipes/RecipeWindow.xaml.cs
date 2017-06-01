1d6d58adc9374bc8c93fd52ab28dc447ace246a5
jjennings <jjennings@student.neumont.edu>
2017-05-31 18:20:12 -0600

    Styling Part 2
    I'd styled the calender.

--- VirtualPantry/Recipes/RecipeWindow.xaml.cs
+++ VirtualPantry/Recipes/RecipeWindow.xaml.cs
@@ -6,10 +6,7 @@ using System.Windows.Media.Imaging;
 using VirtualPantry.Stretch;
 using System.Windows.Controls;
 using VirtualPantry.ThePantry;
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/development
 
 namespace VirtualPantry.Recipes
 {
@@ -60,10 +57,7 @@ namespace VirtualPantry.Recipes
             RecipeListing = path;
         }
 
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/development
         private void homeButton_Clicked(object sender, RoutedEventArgs e)
         {
             main.Show();
@@ -90,15 +84,5 @@ namespace VirtualPantry.Recipes
         {
 
         }
-<<<<<<< HEAD
-=======
-
-        //ImageSource i = new BitmapImage(new Uri("C:\\Users\\Sasuke-Joseph\\Desktop\foodies\\VirtualPantry\\Images\\cat.jpg"));
-
-
-
-        //ImageSource i = new BitmapImage(new Uri("C:\\Users\\Sasuke-Joseph\\Desktop\foodies\\VirtualPantry\\Images\\cat.jpg"));
-
->>>>>>> origin/development
     }
 }
