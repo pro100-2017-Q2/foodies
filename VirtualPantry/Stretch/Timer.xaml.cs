e8655a29c0cf1bc759a820f3981b41d5681234c6
DallasLewis1996 <dallaslewis1996@gmail.com>
2017-05-31 18:44:49 -0600

    IT IS FIXED. PLZ NO BRK

--- VirtualPantry/Stretch/Timer.xaml.cs
+++ VirtualPantry/Stretch/Timer.xaml.cs
@@ -26,12 +26,19 @@ namespace VirtualPantry.Stretch
         private RecipeWindow recipeWindow;
         private DispatcherTimer _timer;
         TimeSpan _time;
-
-        TimeSpan paused;
         private PantryWindow pantry;
         private ConversionChart conversionChart;
 
         TimeSpan _default = new TimeSpan(0,0,0);
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            Duration duration = new Duration(TimeSpan.FromSeconds(20));
+
+            //progress bar animation
+            System.Windows.Media.Animation.DoubleAnimation doubleanimation = new System.Windows.Media.Animation.DoubleAnimation(200.0, duration);
+            ProgressBar.BeginAnimation(ProgressBar.ValueProperty, doubleanimation);
+        }
 
 
         public Timer(MainWindow mainWindow)
@@ -44,19 +51,38 @@ namespace VirtualPantry.Stretch
         }
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-            _time = new TimeSpan(int.Parse(HourLabel.Text), int.Parse(SecondsLabel.Text), 0);
-            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
-            {
-                TimeLeftLabel.Content = _time.ToString("c");
-                if (_time == TimeSpan.Zero) _timer.Stop();
-                _time = _time.Add(TimeSpan.FromSeconds(-1));
-            }, Application.Current.Dispatcher);
+            int hour = int.Parse(HourLabel.Text);
+            int sec = int.Parse(SecondsLabel.Text);
 
-            _timer.Start();
-        }
-        private void PauseButton_Click(object sender, RoutedEventArgs e)
-        {
-            _timer.Stop();
+
+            if (hour < 0)
+            {
+                HourLabel.Text = "0";
+            }
+            if (sec < 0)
+            {
+                SecondsLabel.Text = "1";
+            }
+            try
+            {
+                _time = new TimeSpan(int.Parse(HourLabel.Text), int.Parse(SecondsLabel.Text), 0);
+
+                _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+                {
+                    TimeLeftLabel.Content = _time.ToString("c");
+                    if (_time == TimeSpan.Zero) _timer.Stop();
+                    _time = _time.Add(TimeSpan.FromSeconds(-1));
+                    _timer.Tick += _timer_Tick;
+                }, Application.Current.Dispatcher);
+
+
+                _timer.Start();
+            }
+            catch
+            {
+                HourLabel.Text = "0";
+                SecondsLabel.Text = "0";
+            }
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
@@ -65,6 +91,80 @@ namespace VirtualPantry.Stretch
             TimeLeftLabel.Content = (_time - _time).ToString();
             HourLabel.Text = "0";
             SecondsLabel.Text = "0";
+        }
+
+        private void HourUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int currentHourTime = int.Parse(HourLabel.Text);
+                currentHourTime++;
+                HourLabel.Text = currentHourTime.ToString();
+
+            }
+            catch
+            {
+                HourLabel.Text = "0";
+            }
+        }
+
+        private void HourDownButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int currentHourTime = int.Parse(HourLabel.Text);
+                if (currentHourTime != 0)
+                {
+                    currentHourTime--;
+                }
+                HourLabel.Text = currentHourTime.ToString();
+
+            }
+            catch
+            {
+                HourLabel.Text = "0";
+            }
+        }
+
+        private void SecondsUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int currentSecond = int.Parse(SecondsLabel.Text);
+                currentSecond++;
+                if (currentSecond > 59)
+                {
+                    HourUpButton_Click(sender, e);
+                    currentSecond = 0;
+                }
+                SecondsLabel.Text = currentSecond.ToString();
+            }
+            catch
+            {
+                SecondsLabel.Text = "0";
+            }
+        }
+
+        private void SecondsDownButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int currentSecond = int.Parse(SecondsLabel.Text);
+                if (currentSecond > 0)
+                {
+                    currentSecond--;
+                    if (currentSecond == 0)
+                    {
+                        currentSecond = 59;
+                        HourDownButton_Click(sender, e);
+                    }
+                    SecondsLabel.Text = currentSecond.ToString();
+                }
+            }
+            catch
+            {
+                SecondsLabel.Text = "0";
+            }
         }
 
         private void HomeMenuItem_Click(object sender, RoutedEventArgs e)
@@ -82,6 +182,12 @@ namespace VirtualPantry.Stretch
         private void ConversionChartMenuItem_Click(object sender, RoutedEventArgs e)
         {
             conversionChart.Show();
+            this.Hide();
+        }
+
+        private void homeButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            main.Show();
             this.Hide();
         }
     }
